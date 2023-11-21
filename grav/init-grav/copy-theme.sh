d6404b397a22c001f1758b0b6f15132bb11d1246
SonOfLope <jonathanlopez@hotmail.ca>
2023-11-20 20:06:31 -0500

    updates init and grav

--- grav/init-grav/copy-theme.sh
+++ grav/init-grav/copy-theme.sh
@@ -16,6 +16,8 @@ if [ -z "$(ls -A "$THEMES_DIR")" ]; then
     # Install git-sync plugin
     cd /var/www/html
     bin/gpm install git-sync
+    # Configures git-sync plugin 
+    RUN ln -s /vault/secrets/grav /var/www/html/user/config/plugins/git-sync.yaml
 else
     echo "Themes directory is not empty. Skipping copying initial content."
 fi
