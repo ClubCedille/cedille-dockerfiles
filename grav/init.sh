a8662d9ae9ea256457558bf623387bff4f76d0f5
Simon-Boyer <si.boyer@hotmail.ca>
2023-11-22 21:22:44 -0500

    init script in grav image

---
+++ grav/init.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# copy-theme.sh
+
+sleep 10
+
+THEMES_DIR=/var/www/html/user/themes
+echo " Copying theme..."
+
+# Create the themes directory if it does not exist
+[ ! -d "$THEMES_DIR" ] && mkdir -p "$THEMES_DIR"
+
+# Use rsync to copy the initial content
+if [ -z "$(ls -A "$THEMES_DIR")" ]; then
+    rsync -av /initial-content/ /var/www/html/
+    # Install git-sync plugin
+    cd /var/www/html
+    bin/gpm install git-sync
+    # Configures git-sync plugin 
+    ln -s /vault/secrets/grav /var/www/html/user/config/plugins/git-sync.yaml
+else
+    echo "Themes directory is not empty. Skipping copying initial content."
+fi
+
+if [ -z "$(ls -A /var/www/html/user/.git)" ]; then
+    echo "git not initialized, running init script..."
+    cd /var/www/html
+    rm /var/www/html/user/config/plugins/git-sync.yaml
+    ln -s "/vault/secrets/$GIT_VAULT_SECRET" "/var/www/html/user/config/plugins/git-sync.yaml"
+    git config --global --add safe.directory /var/www/html/user
+    bin/plugin git-sync init
+    echo "done"
+else
+    echo "git already initialized, continuing"
+fi
