0ff8590bef954bd6256df152ec49116df5081a1c
Simon-Boyer <si.boyer@hotmail.ca>
2023-11-25 16:43:09 -0500

    default config

--- grav/entrypoint.sh
+++ grav/entrypoint.sh
@@ -16,4 +16,47 @@ if [ -z "$(ls -A /var/www/html/user/accounts/admin.yaml)" ]; then
     echo done
 fi
 
+if [ -z "$(ls -A /var/www/html/user/config/system.yaml)" ]; then
+    echo "creating default config..."
+    echo <<EOF > /var/www/html/user/config/system.yaml
+home:
+  alias: '/home'
+
+pages:
+  theme: $DEFAULT_THEME
+  process:
+    markdown: true
+    twig: false
+  markdown:
+    extra: false
+
+cache:
+  enabled: false
+  check:
+    method: file
+  driver: auto
+  prefix: 'g'
+
+twig:
+  cache: true
+  debug: true
+  auto_reload: true
+  autoescape: false
+
+assets:
+  css_pipeline: false
+  css_minify: true
+  css_rewrite: true
+  js_pipeline: false
+  js_minify: true
+
+debugger:
+  enabled: false
+  twig: true
+  shutdown:
+    close_connection: true
+EOF
+    echo done
+fi
+
 apache2-foreground
