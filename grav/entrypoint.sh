acb3c9e34240332b891da610a364773a69a65306
Simon-Boyer <si.boyer@hotmail.ca>
2023-11-25 17:15:48 -0500

    admin password with cp not ln

--- grav/entrypoint.sh
+++ grav/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "initialiazing git setup..."
 mkdir -p /var/www/html/user/config/plugins
 cd /var/www/html/
-ln -s "/vault/secrets/$GIT_VAULT_SECRET" "/var/www/html/user/config/plugins/git-sync.yaml"
+cp "/vault/secrets/$GIT_VAULT_SECRET" "/var/www/html/user/config/plugins/git-sync.yaml"
 bin/plugin git-sync init
 bin/plugin git-sync sync > /dev/null
 cd /var/www/html/user
@@ -13,49 +13,6 @@ echo "done"
 if [ -z "$(ls -A /var/www/html/user/accounts/admin.yaml)" ]; then
     echo "Creating admin user..."
     ln -s "/vault/secrets/$ADMIN_VAULT_SECRET" /var/www/html/user/accounts/admin.yaml
-    echo done
-fi
-
-if [ -z "$(ls -A /var/www/html/user/config/system.yaml)" ]; then
-    echo "creating default config..."
-    echo <<EOF > /var/www/html/user/config/system.yaml
-home:
-  alias: '/home'
-
-pages:
-  theme: $DEFAULT_THEME
-  process:
-    markdown: true
-    twig: false
-  markdown:
-    extra: false
-
-cache:
-  enabled: false
-  check:
-    method: file
-  driver: auto
-  prefix: 'g'
-
-twig:
-  cache: true
-  debug: true
-  auto_reload: true
-  autoescape: false
-
-assets:
-  css_pipeline: false
-  css_minify: true
-  css_rewrite: true
-  js_pipeline: false
-  js_minify: true
-
-debugger:
-  enabled: false
-  twig: true
-  shutdown:
-    close_connection: true
-EOF
     echo done
 fi
 
