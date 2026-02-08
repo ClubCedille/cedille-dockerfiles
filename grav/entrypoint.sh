b2e06117d0f2d03aecc616356192039861a82868
SonOfLope <jonathanlopez@hotmail.ca>
2026-02-08 11:59:05 -0500

    recreate symlink

--- grav/entrypoint.sh
+++ grav/entrypoint.sh
@@ -3,13 +3,20 @@
 echo "initialiazing git setup..."
 mkdir -p /var/www/html/user/config/plugins
 cd /var/www/html/
-ln -s "/vault/secrets/$GIT_VAULT_SECRET" "/var/www/html/user/config/plugins/git-sync.yaml"
+
+# Create temporary symlink for git-sync init/sync
+ln -sf "/vault/secrets/$GIT_VAULT_SECRET" "/var/www/html/user/config/plugins/git-sync.yaml"
 bin/plugin git-sync init
 bin/plugin git-sync sync > /dev/null
 cd /var/www/html/user
 git fetch origin
 git reset --hard origin/${HEAD_BRANCH:-main}
 echo "done"
+
+# Re-create symlinks AFTER git reset (which overwrites them with repo content)
+ln -sf "/vault/secrets/$GIT_VAULT_SECRET" /var/www/html/user/config/plugins/git-sync.yaml
+mkdir -p /var/www/html/user/config
+ln -sf /vault/secrets/salt /var/www/html/user/config/security.yaml
 
 if [ ! -f "/var/www/html/user/accounts/admin.yaml" ]; then
   echo "Creating admin user..."
@@ -20,9 +27,5 @@ fi
 echo "Creating sre user..."
 cp "/vault/secrets/$SRE_VAULT_SECRET" /var/www/html/user/accounts/sre.yaml
 echo done
-
-mkdir -p /var/www/html/user/config
-rm -f /var/www/html/user/config/security.yaml
-ln -s /vault/secrets/salt /var/www/html/user/config/security.yaml
 
 apache2-foreground
